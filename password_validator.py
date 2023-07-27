27429a1868fbfec76172778b9fe75b4312ed3c17
fmpberger88 <fabian.berger@protonmail.ch>
2023-07-27 22:03:20 +0200

    finalize streamlit password validator

--- password_validator.py
+++ password_validator.py
@@ -1,3 +1,5 @@
+import random
+import string
 import streamlit as st
 
 
@@ -14,28 +16,29 @@ def password_validator(password):
     contains_lowercase = any(char.islower() for char in password)
 
     if pass_length >= 8 and contains_number and contains_sonderzeichen and contains_uppercase and contains_lowercase:
-        print("Passwort ist stark!")
+        return "Strong"
     elif pass_length >= 8 and (contains_number or contains_sonderzeichen) and contains_uppercase and contains_lowercase:
-        print("Passwort ist gut.")
+        return "Good"
     else:
-        print("Passwort ist schwach!")
+        return "weak"
 
+def generate_password():
+    charset = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    return ''.join(random.choice(charset) for _ in range(8))
 
-# Test
-def main():
-    st.title("Check Password Strength")
-    password = st.text_input("Enter your password:", type="password")
+st.title("Check Password Strength")
+password = st.text_input("Enter your password:", type="password")
 
-    if password:
-        strength = password_validator(password)
-        st.write(f'Password strength: {strength.capitalize()}')
+if password:
+    strength = password_validator(password)
+    st.write(f'Password strength: {strength.title()}')
 
-    st.subheader("Password Hints:")
-    st.markdown("- The password should be at least 8 characters long.")
-    st.markdown("- The password should contain both uppercase and lowercase letters.")
-    st.markdown("- The password should have at least one number.")
-    st.markdown("- The password should have at least one special character (e.g., #, !, $, %, etc.).")
+st.subheader("Password Hints:")
+st.markdown("- The password should be at least 8 characters long.")
+st.markdown("- The password should contain both uppercase and lowercase letters.")
+st.markdown("- The password should have at least one number.")
+st.markdown("- The password should have at least one special character (e.g., #, !, $, %, etc.).")
 
-
-if __name__ == "__main__":
-    main()
+if st.button('Generate Random Password'):
+    random_password = generate_password()
+    st.write("Your generated password:", random_password)
